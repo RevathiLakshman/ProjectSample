7eb45376947464940b45d59145ced058e50493e2
RevathiLakshman <RevathiLakshman23@gmail.com>
2022-07-19 22:00:27 -0700

    rev code

--- src/test/java/org/sample/ProjectSample.java
+++ src/test/java/org/sample/ProjectSample.java
@@ -4,6 +4,9 @@ public class ProjectSample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
 
 	}
 
